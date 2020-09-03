e62625cf05de906bfb979ac030fef93758517def
Delfia <lovemyself.11.jl@gmail.com>
2020-09-03 13:51:14 +0530

    12th commit

---
+++ demo.js
@@ -0,0 +1,34 @@
+let data = [
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k2f1n680/headphone/stero-dynamic/j/e/5/boat-rockerz-on-ear-400-original-imafg95zbguyzbf5.jpeg?q=70",
+    "name":"boat headphones"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k6zda4w0/shoe/f/t/v/sx0604g-8-sparx-navybluered-original-imafpbmqwadvhgmr.jpeg?q=70",
+    "name":"good shoes"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k0h12fk0/computer/j/y/r/hp-na-thin-and-light-laptop-original-imafk9p4wpuxk3hy.jpeg?q=70",
+    "name":"nice laptop"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/ja73ki80/webcam/pc-webcam/7/q/e/logitech-c310-original-imaeztzqny7jh7gh.jpeg?q=70",
+    "name":"webcam"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k6mibgw0/datacard/r/h/g/jiofi-jmr-541-original-imafpfhandhkptwc.jpeg?q=70",
+    "name":"Jio"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/allinone-desktop/b/t/f/lenovo-aio300-original-imaescyqhsuehbvb.jpeg?q=70",
+    "name":"Desktop"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k4ehnrk0/mobile-holder/6/s/g/aluminium-adjustable-and-foldable-dock-elv-original-imafnb843mpzvghd.jpeg?q=70",
+    "name":"Iphone charger"
+    },
+    {
+    "img": "https://rukminim1.flixcart.com/image/150/150/k5lcvbk0pkrrdj/headphone/dynamic/z/j/h/jbl-c150siublk-original-imafeyby52kxehz2.jpeg?q=70",
+    "name":"JBL headphones"
+    }
+    ]
